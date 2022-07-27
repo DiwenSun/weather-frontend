010c24f3446177cfb0e5cc9e2129164de34d055b
sdw <diwen.sun@curio.health>
2022-07-27 14:11:01 +0800

    improve

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,28 @@ function App() {
 
     return (
         <div className="App">
-            {/*<header className="App-header">*/}
-            {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-            {/*  <p>*/}
-            {/*    Edit <code>src/App.tsx</code> and save to reload.*/}
-            {/*  </p>*/}
-            {/*  <a*/}
-            {/*    className="App-link"*/}
-            {/*    href="https://reactjs.org"*/}
-            {/*    target="_blank"*/}
-            {/*    rel="noopener noreferrer"*/}
-            {/*  >*/}
-            {/*    Learn React*/}
-            {/*  </a>*/}
-            {/*</header>*/}
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              {/*<p>*/}
+              {/*  Edit <code>src/App.tsx</code> and save to reload.*/}
+              {/*</p>*/}
+              {/*<a*/}
+              {/*  className="App-link"*/}
+              {/*  href="https://reactjs.org"*/}
+              {/*  target="_blank"*/}
+              {/*  rel="noopener noreferrer"*/}
+              {/*>*/}
+              {/*  Learn React*/}
+              {/*</a>*/}
+            </header>
+
+            <br/>
+
             <Weather/>
 
             <br/>
 
-            <div>
+            <div className={"clicks"}>
                 <p>You clicked {count} times</p>
                 <button onClick={() => setCount(count + 1)}>
                     Click me
